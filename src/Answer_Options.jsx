d73b31be35929a4c0e77aa5c6b7833b178030dd8
Tymm Schmitke <ifellinaholeonce@gmail.com>
2018-01-26 20:59:51 +0000

    scared of detached head

---
+++ src/Answer_Options.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class Answer_Options extends Component {
+  render() {
+    return (
+      <div>
+      <img src="http://fillmurray.com/g/100/100"/>
+      <img src="http://fillmurray.com/g/100/100"/>
+      <img src="http://fillmurray.com/g/100/100"/>
+      </div>
+    );
+  }
+}
+
+export default Answer_Options;
