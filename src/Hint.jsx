d96451a4faeb085ffe1c81f95b6477b2bf039681
Mandy <mandycheang135@gmail.com>
2018-01-26 20:40:17 +0000

    Create hintList, and make setstate in App component

--- src/Hint.jsx
+++ src/Hint.jsx
@@ -1,38 +1,13 @@
 import React, {Component} from 'react';
 
 class Hint extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      hints: ['hint1','hint2','hint3'],
-      numHints: 0
-    }
-  }
-
-  toggleHint = (e) => {
-    let newHints = this.state.numHints + 1;
-    this.setState({numHints: newHints})
-
-  }
-
   render() {
     console.log("Rendering <Hint/>");
-
-    const hints = this.state.hints.filter((hint, i) => {
-      return i < this.state.numHints
-    }).map((hint,i) => {
-      return <p key={i}>{hint}</p>
-    })
-
     return (
-      <div>
-        <button onClick={this.toggleHint}> HINT: </button>
-        <div className="hints">
-          {hints}
+       <div className="Hint">
+          <span className="hint-content">{this.props.hint.content}</span>
         </div>
-      </div>
     );
   }
 }
-
 export default Hint;
